259be2f1148074c7a593b0ae45b625acfadb1b07
deepak-5454 <111001426+deepak-5454@users.noreply.github.com>
2023-09-06 19:43:41 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+def my_function(fname):
+    print(fname + "refsnes")
+
+my_function("Email")
+my_function("Tobias")
+my_function("Linus")
+    
